9f5416203e008df13e53984c33c0b7a9c4986226
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2025-07-18 23:19:27 -0300

    feat: add padding

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,6 +1,6 @@
 const Copyright = () => {
   return (
-    <div className="text-center bg-primary-700 text-white flex justify-center items-center py-6">
+    <div className="text-center bg-primary-700 text-white flex justify-center items-center py-6 px-4">
       <p>Copyright © 2025 Grupo Santa Joana. Todos os direitos reservados.</p>
     </div>
   );
